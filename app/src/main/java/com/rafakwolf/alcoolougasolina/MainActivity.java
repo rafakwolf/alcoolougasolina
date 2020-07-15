22ba5cfdb75af7e40158a5ba6605cb7074f19f1f
Rafael <rafakwolf@gmail.com>
2020-07-14 22:19:16 -0300

    Implementando calculo

--- app/src/main/java/com/rafakwolf/alcoolougasolina/MainActivity.java
+++ app/src/main/java/com/rafakwolf/alcoolougasolina/MainActivity.java
@@ -3,12 +3,38 @@ package com.rafakwolf.alcoolougasolina;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText editAlcool;
+    private EditText editGasolina;
+    private TextView textResultado;
+
+    public void calcularMelhorOpcao(View view) {
+        Double precoAlcool = Double.parseDouble(editAlcool.getText().toString());
+        Double precoGasolina = Double.parseDouble(editGasolina.getText().toString());
+
+        double resultado = precoAlcool / precoGasolina;
+
+        if (resultado >= 0.7) {
+            textResultado.setText(R.string.resultadoAlcool);
+        } else {
+            textResultado.setText(R.string.resultadoGasolina);
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        editAlcool = findViewById(R.id.editTextNumberAlcool);
+        editGasolina = findViewById(R.id.editTextNumberGasolina);
+
+        textResultado = findViewById(R.id.textViewResultado);
     }
 }
